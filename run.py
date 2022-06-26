10cc84e2dae26e707a0053b9380a4b44f58ad8df
kartikbehlHJ <kartik.behl@heyjobs.de>
2022-06-26 23:41:22 +0530

    Update xpaths and selenium

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -20,9 +21,9 @@ def login(driver, wait):
     wait.until(presence_of_element_located(
         (By.XPATH, elements["loginButton"])))
 
-    login_input = driver.find_element_by_xpath(elements["loginInput"])
-    password_input = driver.find_element_by_xpath(elements["passwordInput"])
-    login_button = driver.find_element_by_xpath(elements["loginButton"])
+    login_input = driver.find_element(By.XPATH, elements["loginInput"])
+    password_input = driver.find_element(By.XPATH, elements["passwordInput"])
+    login_button = driver.find_element(By.XPATH, elements["loginButton"])
 
     login_input.send_keys(data["username"])
     password_input.send_keys(data["password"])
@@ -33,39 +34,36 @@ def login(driver, wait):
 
 def close_popup(driver, wait):
     wait.until(presence_of_element_located((By.XPATH, elements["popUp"])))
-    driver.find_element_by_xpath(elements["popUp"]).click()
+    driver.find_element(By.XPATH, elements["popUp"]).click()
 
 
 def open_profile(driver, wait):
-    wait.until(presence_of_element_located(
-        (By.XPATH, elements["profileLink"])))
-    driver.find_element_by_xpath(elements["profileLink"]).click()
+    driver.get(f'https://instagram.com/{data["username"]}')
 
 
-def load_list(driver, wait, num):
-    wait.until(presence_of_element_located((By.XPATH, elements["list"])))
+def load_list(driver, wait, num, list_xpath):
+    wait.until(presence_of_element_located((By.XPATH, list_xpath)))
 
     current_count = 0
 
     while current_count < num - 1:
-        wait.until(presence_of_element_located((By.XPATH, elements["list"])))
-        current_list = driver.find_elements_by_xpath(elements["list"])
+        wait.until(presence_of_element_located((By.XPATH, list_xpath)))
+        current_list = driver.find_elements(By.XPATH, list_xpath)
 
         current_count = len(current_list)
 
-        xpath = elements["list"] + f"[{current_count}]"
+        xpath = list_xpath + f"[{current_count}]"
         wait.until(presence_of_element_located((By.XPATH, xpath)))
-        element = driver.find_element_by_xpath(xpath)
+        element = driver.find_element(By.XPATH, xpath)
 
         print(current_count)
         ActionChains(driver).move_to_element(element).perform()
-        time.sleep(1)
-        # current_count += 1
+        time.sleep(0.5)
 
 
 def open_list(driver, wait, xpath):
     wait.until(presence_of_element_located((By.XPATH, xpath)))
-    driver.find_element_by_xpath(xpath).click()
+    driver.find_element(By.XPATH, xpath).click()
 
 
 def get_ids(driver, wait, category):
@@ -73,18 +71,18 @@ def get_ids(driver, wait, category):
     xpath_num = elements[category + "Num"]
 
     wait.until(presence_of_element_located((By.XPATH, xpath_link)))
-    num = int(driver.find_element_by_xpath(xpath_num).text)
+    num = int(driver.find_element(By.XPATH, xpath_num).text)
 
     open_list(driver, wait, xpath_link)
-    load_list(driver, wait, num)
+    load_list(driver, wait, num, elements[category + 'List'])
 
-    list_elements = driver.find_elements_by_xpath(elements["list"])
+    list_elements = driver.find_elements(By.XPATH, elements[category + "List"])
     user_list = list()
     for i in range(len(list_elements)):
         print(i)
-        xpath_id = elements["list"] + \
-            f"[{str(i + 1)}]" + "/div/div/div[2]/div/a"
-        user_name = driver.find_element_by_xpath(xpath_id).text
+        xpath_id = elements[category + "List"] + \
+            f"[{str(i + 1)}]" + "/div/div[1]/div[2]/div[1]/span/a/span"
+        user_name = driver.find_element(By.XPATH, xpath_id).text
         user_list.append(user_name)
 
     return user_list
@@ -95,27 +93,33 @@ def unfollow(ids):
 
 
 def main():
-    with webdriver.Chrome() as driver:
-        wait = WebDriverWait(driver, 10)
-        driver.maximize_window()
-        driver.get("https://instagram.com")
+    chromedriver = "./chromedriver"
+    option = webdriver.ChromeOptions()
+    option.binary_location = '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser'
+    s = Service(chromedriver)
+    driver = webdriver.Chrome(service=s, options=option)
+    wait = WebDriverWait(driver, 10)
+    driver.maximize_window()
+    driver.get("https://instagram.com")
 
-        login(driver, wait)
-        close_popup(driver, wait)
-        open_profile(driver, wait)
+    login(driver, wait)
+    close_popup(driver, wait)
+    open_profile(driver, wait)
 
-        followers_list = get_ids(driver, wait, "followers")
-        driver.back()
-        following_list = get_ids(driver, wait, "following")
+    followers_list = get_ids(driver, wait, "followers")
+    driver.back()
+    following_list = get_ids(driver, wait, "following")
 
-        not_following = list()
-        for id in following_list:
-            if id not in followers_list:
-                not_following.append(id)
+    driver.close()
 
-        with open("./ids.txt", "w") as f:
-            for id in not_following:
-                f.write(id + "\n")
+    not_following = list()
+    for id in following_list:
+        if id not in followers_list:
+            not_following.append(id)
+
+    with open("./ids.txt", "w") as f:
+        for id in not_following:
+            f.write(id + "\n")
 
 
 if __name__ == "__main__":
